edee4194d5418c76267ff5190945add18a8d7729
Mohamed Adel <mohamedadelzmd@gmail.com>
2024-08-11 08:41:52 +0300

    move highlight on: hover, scroll, scrollToSection

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useReducer, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import styles from "./Navbar.module.css";
 
 const initialHighlight = {
@@ -6,6 +6,131 @@ const initialHighlight = {
   highlightPosition: 58,
   highlightWidth: 100,
 };
+
+function Navbar({
+  onScrollToSection,
+  headerRef,
+  skillsRef,
+  projectsRef,
+  contactRef,
+}) {
+  const [{ activeSection, highlightPosition, highlightWidth }, dispatch] =
+    useReducer(reducer, initialHighlight);
+  const [isScrolling, setIsScrolling] = useState(false);
+  const [hoveredElement, setHoveredElement] = useState(null);
+  const timeoutId = useRef(null);
+  const [sections, setSections] = useState([]);
+  const ulElements = [
+    ["home", headerRef],
+    ["skills", skillsRef],
+    ["projects", projectsRef],
+    ["contact", contactRef],
+  ];
+
+  useEffect(() => {
+    // This will run after the component has mounted and the DOM is ready
+    const sectionElements = document.querySelectorAll("section");
+    setSections(sectionElements);
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  useEffect(() => {
+    // This handles changing the highligted section on scroll
+    function handleScroll() {
+      if (!isScrolling) {
+        let currentSection = "home";
+
+        currentSection = hoveredElement
+          ? hoveredElement
+          : sectionInView(sections, currentSection);
+
+        dispatch({ type: currentSection });
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isScrolling]);
+
+  const handleMouseEnter = (section) => {
+    setHoveredElement(section);
+    if (!isScrolling) {
+      dispatch({ type: section });
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredElement(null);
+    if (!isScrolling) {
+      let currentSection = "home";
+      currentSection = sectionInView(sections, currentSection);
+      dispatch({ type: currentSection });
+    }
+  };
+
+  const handleLinkClick = (section) => {
+    if (timeoutId.current) {
+      clearTimeout(timeoutId.current);
+    }
+    const currentSection = section.current.getAttribute("id");
+    const allowScrollDelay = Math.min(
+      570,
+      Math.abs(
+        (section.current.offsetTop - window.scrollY) / (window.scrollY || 1)
+      ) * 700
+    );
+
+    dispatch({ type: currentSection });
+    setIsScrolling(true);
+    onScrollToSection(section);
+
+    timeoutId.current = setTimeout(() => {
+      setIsScrolling(false);
+    }, allowScrollDelay);
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <div
+        className="highlight"
+        style={{
+          left: `${highlightPosition}px`,
+          width: `${highlightWidth}px`,
+        }}
+      />
+      <ul>
+        {ulElements.map(([name, ref]) => (
+          <li
+            key={name}
+            className={name === activeSection ? "active" : ""}
+            onMouseEnter={() => handleMouseEnter(name)}
+            onMouseLeave={handleMouseLeave}
+            onClick={() => handleLinkClick(ref)}
+          >
+            <button>{name.charAt(0).toUpperCase() + name.slice(1)}</button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
+function sectionInView(sections, currentSection) {
+  const middle = window.scrollY + document.documentElement.clientHeight / 3;
+  let currentSectionTop;
+  for (let i = sections.length - 1; i >= 0; i--) {
+    currentSectionTop = sections[i].offsetTop;
+    if (middle >= currentSectionTop) {
+      currentSection = sections[i].id;
+      break;
+    }
+  }
+  return currentSection;
+}
 
 function reducer(state, action) {
   switch (action.type) {
@@ -32,130 +157,4 @@ function reducer(state, action) {
         highlightWidth: 120,
       };
   }
-}
-
-function Navbar({
-  onScrollToSection,
-  headerRef,
-  skillsRef,
-  projectsRef,
-  contactRef,
-}) {
-  const [{ activeSection, highlightPosition, highlightWidth }, dispatch] =
-    useReducer(reducer, initialHighlight);
-  const [scrollAllowed, setScrollAllowed] = useState(true);
-
-  const sections = useMemo(() => {
-    return document.querySelectorAll("section");
-  }, []);
-
-  useEffect(() => {
-    function handleScroll() {
-      if (scrollAllowed) {
-        let currentSection = "home";
-
-        currentSection = sectionInView(sections, currentSection);
-
-        dispatch({ type: currentSection });
-      }
-    }
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [scrollAllowed]);
-
-  const handleMouseEnter = (section) => {
-    dispatch({ type: section });
-  };
-
-  const handleMouseLeave = () => {
-    if (scrollAllowed) {
-      let currentSection = "home";
-      currentSection = sectionInView(sections, currentSection);
-
-      dispatch({ type: currentSection });
-    }
-  };
-
-  const handleLinkClick = (section) => {
-    clearTimeout();
-    const currentSection = section.current.getAttribute("id");
-    const allowScrollDelay = Math.min(
-      570,
-      Math.abs(
-        (section.current.offsetTop - window.scrollY) / (window.scrollY || 1)
-      ) * 700
-    );
-
-    dispatch({ type: currentSection });
-    setScrollAllowed(false);
-    onScrollToSection(section);
-
-    setTimeout(() => {
-      setScrollAllowed(true);
-    }, allowScrollDelay);
-  };
-
-  return (
-    <nav className={styles.navbar}>
-      <div
-        className="highlight"
-        style={{
-          left: `${highlightPosition}px`,
-          width: `${highlightWidth}px`,
-        }}
-      />
-      <ul>
-        <li
-          onClick={() => handleLinkClick(headerRef)}
-          className={activeSection === "home" ? "active" : ""}
-          onMouseEnter={() => handleMouseEnter("home")}
-          onMouseLeave={handleMouseLeave}
-        >
-          <button>Home</button>
-        </li>
-        <li
-          onClick={() => handleLinkClick(skillsRef)}
-          className={activeSection === "skills" ? "active" : ""}
-          onMouseEnter={() => handleMouseEnter("skills")}
-          onMouseLeave={handleMouseLeave}
-        >
-          <button>Skills</button>
-        </li>
-        <li
-          onClick={() => handleLinkClick(projectsRef)}
-          className={activeSection === "projects" ? "active" : ""}
-          onMouseEnter={() => handleMouseEnter("projects")}
-          onMouseLeave={handleMouseLeave}
-        >
-          <button>Projects</button>
-        </li>
-        <li
-          onClick={() => handleLinkClick(contactRef)}
-          className={activeSection === "contact" ? "active" : ""}
-          onMouseEnter={() => handleMouseEnter("contact")}
-          onMouseLeave={handleMouseLeave}
-        >
-          <button>Contact</button>
-        </li>
-      </ul>
-    </nav>
-  );
-}
-
-export default Navbar;
-function sectionInView(sections, currentSection) {
-  const middle = window.scrollY + document.documentElement.clientHeight / 3;
-  let currentSectionTop;
-  for (let i = sections.length - 1; i >= 0; i--) {
-    currentSectionTop = sections[i].offsetTop;
-    if (middle >= currentSectionTop) {
-      currentSection = sections[i].id;
-      break;
-    }
-  }
-  return currentSection;
 }
